9bc760789089ad149b6840d520579e1b16e1a700
April Liwanag <ajmliwanag@gmail.com>
2022-09-05 22:54:56 +0200

    my first java program

---
+++ Agile-Java/BasicsJava/src/VariablesOperatorsOpdracht1.java
@@ -0,0 +1,10 @@
+public class VariablesOperatorsOpdracht1 {
+
+//    Opdracht 1
+//    int used to declare variables
+//    long used to hold the larger whole number that int cannot hold
+//    double used to represent floating-point numbers
+//    float used when you want to save memory and when calculations do not require more than 6 or 7 digits of precision
+
+
+}
---
+++ Agile-Java/BasicsJava/src/VariablesOperatorsOpdracht2.java
@@ -0,0 +1,5 @@
+public class VariablesOperatorsOpdracht2 {
+    public static void main(String[] args) {
+
+    }
+}
